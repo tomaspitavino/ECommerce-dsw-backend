ef3dc59334d4997ad8dbf7d10f9dfd11a1998de6
lordagustin22 <niconecrofilo589@gmail.com>
2025-05-23 01:09:57 -0300

    Ejemplo de server con express

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
-const muebleRoutes = require('./routes/muebleRoutes');
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import muebleRoutes from "./routes/muebleRoutes.js";
 
 const app = express();
 
 // Middlewares
 app.use(cors()); // Para permitir peticiones desde el frontend
-app.use(morgan('dev')); // Logger de peticiones
+app.use(morgan("dev")); // Logger de peticiones
 app.use(express.json()); // Para parsear JSON en las peticiones
 
 // Rutas
-app.use('/api/muebles', muebleRoutes);
+app.use("/api/muebles", muebleRoutes);
 
 // Manejador de errores
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: 'Algo salió mal!' });
+  res.status(500).json({ error: "Algo salió mal!" });
 });
 
 const PORT = process.env.PORT || 3000;
