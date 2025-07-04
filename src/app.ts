a3b60cca7e276695400cd8f183f959890c7c225e
lordagustin22 <niconecrofilo589@gmail.com>
2025-07-04 12:53:51 -0300

    Actualizacion de CRUD

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
-import {RequestContext} from '@mikro-orm/core';
-import express from 'express';
-import 'reflect-metadata';
-import {categoriaRouter} from './categoria/categoria.routes.js';
-import {clienteRouter} from './cliente/cliente.routes.js';
-import {materialRouter} from './material/material.routes.js';
-import {muebleRouter} from './mueble/mueble.routes.js';
-import {orm, syncSchema} from './shared/db/orm.js';
+import express from "express";
+import "reflect-metadata";
+import { RequestContext } from "@mikro-orm/core";
+import { categoriaRouter } from "./categoria/categoria.routes.js";
+import { clienteRouter } from "./cliente/cliente.routes.js";
+import { materialRouter } from "./material/material.routes.js";
+import { muebleRouter } from "./mueble/mueble.routes.js";
+import { orm, syncSchema } from "./shared/db/orm.js";
 // import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-	RequestContext.create(orm.em, next);
+  RequestContext.create(orm.em, next);
 });
 
 /*
@@ -26,17 +26,17 @@ app.use(
 
 const port = 3000;
 
-app.use('/api/clientes', clienteRouter);
-app.use('/api/categorias', categoriaRouter);
-app.use('/api/materiales', materialRouter);
-app.use('/api/muebles', muebleRouter);
+app.use("/api/clientes", clienteRouter);
+app.use("/api/categorias", categoriaRouter);
+app.use("/api/materiales", materialRouter);
+app.use("/api/muebles", muebleRouter);
 
 app.use((_, res) => {
-	res.status(404).send({message: 'Ruta no encontrada'});
+  res.status(404).send({ message: "Ruta no encontrada" });
 });
 
 await syncSchema(); // never in production
 
 app.listen(port, () => {
-	console.log(`Listening on http://localhost:${port}/`);
+  console.log(`Listening on http://localhost:${port}/`);
 });
