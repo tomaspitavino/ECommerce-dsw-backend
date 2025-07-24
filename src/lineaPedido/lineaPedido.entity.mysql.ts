135c3aea0937003e305526fa465e106451f9ebcd
lordagustin22 <niconecrofilo589@gmail.com>
2025-07-23 23:25:47 -0300

    Revisar esta entidad en el diagrama de clases, ya que parece ser una entidad de relación entre Mueble y Pedido

--- src/lineaPedido/lineaPedido.entity.mysql.ts
+++ src/lineaPedido/lineaPedido.entity.mysql.ts
@@ -1,12 +1,11 @@
 import {
-  Cascade,
-  Collection,
-  Entity,
-  ManyToMany,
-  ManyToOne,
-  OneToMany,
-  Property,
-  Rel
+    Cascade,
+    Collection,
+    Entity,
+    ManyToOne,
+    OneToMany,
+    Property,
+    Rel
 } from "@mikro-orm/core";
 import { BaseEntity } from "../shared/db/baseEntity.entity.mysql.js";
 import { Pedido } from "../pedido/pedido.entity.mysql.js";
@@ -17,21 +16,21 @@ import { Mueble } from "../mueble/mueble.entity.mysql.js";
 
 @Entity()
 export class lineaPedido extends BaseEntity {
-  // Valor calculado de lineaPedido.cantidad * mueble.precioUnitario
-  @Property({ nullable: false })
-  subtotal!: number;
+    // Valor calculado de lineaPedido.cantidad * mueble.precioUnitario
+    @Property({ nullable: false })
+    subtotal!: number;
 
-  // Deberia de iniciar con un estado por defecto, como "pendiente" o "en proceso"
-  @Property({ nullable: false })
-  estado!: string;
+    // Deberia de iniciar con un estado por defecto, como "pendiente" o "en proceso"
+    @Property({ nullable: false })
+    estado!: string;
 
-  // Deberia iniciar con 1 ya que es la cantidad por defecto de un producto en un pedido
-  @Property({ nullable: false })
-  cantidad!: number;
+    // Deberia iniciar con 1 ya que es la cantidad por defecto de un producto en un pedido
+    @Property({ nullable: false })
+    cantidad!: number;
 
-  @OneToMany(() => Mueble, (mueble) => mueble.lineaPedido, {nullable: false, cascade: [Cascade.ALL]})
-  mueble = new Collection<Mueble>(this);
+    @OneToMany(() => Mueble, (mueble) => mueble.lineaPedido, { nullable: false, cascade: [Cascade.ALL] })
+    mueble = new Collection<Mueble>(this);
 
-  @ManyToOne(() => Pedido, { nullable: false })
-  pedido!: Rel<Pedido>;
+    @ManyToOne(() => Pedido, { nullable: false })
+    pedido!: Rel<Pedido>;
 }
