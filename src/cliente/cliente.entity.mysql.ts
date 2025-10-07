a7107127052ab31a92126345cdfef3d3b08cdb5a
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-07 16:34:55 -0300

    Eliminar descuento (es de AD)

--- src/cliente/cliente.entity.mysql.ts
+++ src/cliente/cliente.entity.mysql.ts
@@ -1,60 +1,60 @@
 import {
-    Cascade,
-    Collection,
-    Entity,
-    ManyToMany,
-    OneToMany,
-    Property,
-} from '@mikro-orm/core';
-import { Pedido } from '../pedido/pedido.entity.mysql.js';
-import { BaseEntity } from '../shared/db/baseEntity.entity.mysql.js';
-import { Descuento } from '../descuento/descuento.entity.mysql.js';
+  Cascade,
+  Collection,
+  Entity,
+  ManyToMany,
+  OneToMany,
+  Property,
+} from "@mikro-orm/core";
+import { Pedido } from "../pedido/pedido.entity.mysql.js";
+import { BaseEntity } from "../shared/db/baseEntity.entity.mysql.js";
+import { Descuento } from "../descuento/descuento.entity.mysql.js";
 
 @Entity()
 export class Cliente extends BaseEntity {
-    @Property({ nullable: false })
-    nombre!: string;
+  @Property({ nullable: false })
+  nombre!: string;
 
-    @Property({ nullable: false })
-    apellido!: string;
+  @Property({ nullable: false })
+  apellido!: string;
 
-    @Property({ nullable: false, unique: true })
-    direccion!: string;
+  @Property({ nullable: false, unique: true })
+  direccion!: string;
 
-    @Property({ nullable: false, unique: true })
-    telefono!: string;
+  @Property({ nullable: false, unique: true })
+  telefono!: string;
 
-    @Property({ nullable: false, unique: true })
-    dni!: string;
+  @Property({ nullable: false, unique: true })
+  dni!: string;
 
-    @Property({ nullable: false, unique: true })
-    usuario!: string;
+  @Property({ nullable: false, unique: true })
+  usuario!: string;
 
-    @Property({ nullable: false, unique: true })
-    email!: string;
+  @Property({ nullable: false, unique: true })
+  email!: string;
 
-    @Property({ nullable: false })
-    contrasenia!: string;
+  @Property({ nullable: false })
+  contrasenia!: string;
 
-    @OneToMany(() => Pedido, (pedidos) => pedidos.cliente, {
-        cascade: [Cascade.ALL],
-    })
-    pedidos = new Collection<Pedido>(this);
+  @OneToMany(() => Pedido, (pedidos) => pedidos.cliente, {
+    cascade: [Cascade.ALL],
+  })
+  pedidos = new Collection<Pedido>(this);
 
-    @Property()
-    fondos: number = 0;
+  @Property()
+  fondos: number = 0;
 
-    @Property()
-    puntos: number = 0;
+  @Property()
+  puntos: number = 0;
 
-    // En realidad favoritos es una relacion con atributos, deberia de ser una entidad
-    // Revisar el diagrama
-    @Property({ nullable: true })
-    favoritos?: string[]; // Relación many-to-many con Mueble, se puede definir como una colección si se usa un ORM
+  // En realidad favoritos es una relacion con atributos, deberia de ser una entidad
+  // Revisar el diagrama
+  @Property({ nullable: true })
+  favoritos?: string[]; // Relación many-to-many con Mueble, se puede definir como una colección si se usa un ORM
 
-    @ManyToMany(() => Descuento, (descuento) => descuento.clientes, {
-        cascade: [Cascade.ALL],
-        owner: true,
-    })
-    descuentos!: Descuento[];
+  // @ManyToMany(() => Descuento, (descuento) => descuento.clientes, {
+  //     cascade: [Cascade.ALL],
+  //     owner: true,
+  // })
+  // descuentos!: Descuento[];
 }
