cffbf825c0f9f889246cb47dc9f6d8861b7205f7
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-07 16:36:11 -0300

    Uso de cors en el archivo env para gestiones de peticiones cruzadas

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,52 @@
-import {RequestContext} from '@mikro-orm/core';
-import express from 'express';
-import 'reflect-metadata';
-import {categoriaRouter} from './categoria/categoria.routes.js';
-import {clienteRouter} from './cliente/cliente.routes.js';
-import {descuentoRouter} from './descuento/descuento.routes.js';
-import {lineaPedidoRouter} from './lineaPedido/lineaPedido.routes.js';
-import {materialRouter} from './material/material.routes.js';
-import {muebleRouter} from './mueble/mueble.routes.js';
-import {orm, syncSchema} from './shared/db/orm.js';
-// import cors from 'cors';
+import { RequestContext } from "@mikro-orm/core";
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+import "express-async-errors";
+import "reflect-metadata";
+import { categoriaRouter } from "./categoria/categoria.routes.js";
+import { clienteRouter } from "./cliente/cliente.routes.js";
+import { lineaPedidoRouter } from "./lineaPedido/lineaPedido.routes.js";
+import { materialRouter } from "./material/material.routes.js";
+import { muebleRouter } from "./mueble/mueble.routes.js";
+import { orm, syncSchema } from "./shared/db/orm.js";
+// import { AppError, errorHandler } from './shared/errors';
 
 const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-	RequestContext.create(orm.em, next);
+  RequestContext.create(orm.em, next);
 });
 
-/*
 app.use(
-    cors({
-        origin: '*', // Permitir todas las solicitudes de origen cruzado
-        credentials: true, // Permitir credenciales (cookies, autenticación HTTP, etc.)
-    })
+  cors({
+    origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(",") : "*",
+    credentials: true,
+  }),
 );
-*/
 
 const port = 3000;
 
 // Revisar paths
-app.use('/api/clientes', clienteRouter);
-app.use('/api/categorias', categoriaRouter);
-app.use('/api/materiales', materialRouter);
-app.use('/api/muebles', muebleRouter);
-app.use('/api/lineas-pedido', lineaPedidoRouter);
-app.use('/api/descuentos', descuentoRouter);
+app.use("/api/clientes", clienteRouter);
+app.use("/api/categorias", categoriaRouter);
+app.use("/api/materiales", materialRouter);
+app.use("/api/muebles", muebleRouter);
+app.use("/api/lineas-pedido", lineaPedidoRouter);
+// descuento tiene alcance de aprobación directa, lo sacamos para la regularidad.
 
-app.use((_, res) => {
-	res.status(404).send({message: 'Ruta no encontrada'});
-});
+/* // 404 para rutas no existentes
+app.use('*', (_req, _res, next) =>
+    next(new AppError('Recurso no encontrado', 404))
+);
+
+// Manejador de errores global (siempre al final)
+app.use(errorHandler);
+ */
 
 await syncSchema(); // never in production
 
 app.listen(port, () => {
-	console.log(`Listening on http://localhost:${port}/`);
+  console.log(`Listening on http://localhost:${port}/`);
 });
