6b0daa2027d2f867a75d159e5990984e05881348
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-07 22:27:48 -0300

    algunos parametros de orm se ocultan en dotenv

--- src/shared/db/orm.ts
+++ src/shared/db/orm.ts
@@ -1,14 +1,20 @@
-import {MikroORM} from '@mikro-orm/core';
-import {SqlHighlighter} from '@mikro-orm/sql-highlighter';
+import { MikroORM } from '@mikro-orm/core';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const orm = await MikroORM.init({
 	entities: ['dist/**/*.entity.mysql.js'],
 	entitiesTs: ['src/**/*.entity.mysql.ts'],
-	dbName: 'muebleria',
+	// dbName: 'muebleria',
+	dbName: process.env.DB_NAME,
 	type: 'mysql',
-	clientUrl: 'mysql://dsw:dsw@127.0.0.1:3306/muebleria',
+	// clientUrl: 'mysql://dsw:dsw@127.0.0.1:3306/muebleria',
+	clientUrl: process.env.DB_URL, // Ej: mysql://user:pass@host:port/db
 	highlighter: new SqlHighlighter(),
-	debug: true,
+	// debug: true,
+	debug: process.env.NODE_ENV !== 'production',
 	schemaGenerator: {
 		// never use in production
 		disableForeignKeys: true,
