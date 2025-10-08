22e62dcee16c206832e3930dfd28d7120eb0a5f1
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-07 22:24:36 -0300

    Agregado ejemplos de materiales, muebles y categorias

--- src/mueble/mueble.entity.mysql.ts
+++ src/mueble/mueble.entity.mysql.ts
@@ -1,34 +1,45 @@
-import {Entity, ManyToOne, Property, Rel} from '@mikro-orm/core';
-import {Categoria} from '../categoria/categoria.entity.mysql.js';
-import {lineaPedido} from '../lineaPedido/lineaPedido.entity.mysql.js';
-import {Material} from '../material/material.entity.mysql.js';
-import {BaseEntity} from '../shared/db/baseEntity.entity.mysql.js';
+import {
+	Collection,
+	Entity,
+	ManyToMany,
+	ManyToOne,
+	Property,
+	Rel,
+} from '@mikro-orm/core';
+import { Categoria } from '../categoria/categoria.entity.mysql.js';
+import { Cliente } from '../cliente/cliente.entity.mysql.js';
+import { lineaPedido } from '../lineaPedido/lineaPedido.entity.mysql.js';
+import { Material } from '../material/material.entity.mysql.js';
+import { BaseEntity } from '../shared/db/baseEntity.entity.mysql.js';
 
 @Entity()
 export class Mueble extends BaseEntity {
-	@Property({nullable: false})
+	@Property({ nullable: false })
 	descripcion!: string;
 
-	@Property({nullable: false})
+	@Property({ nullable: false })
 	stock!: number;
 
-	@Property({nullable: false})
+	@Property({ nullable: false })
 	etiqueta!: string;
 
-	@Property({nullable: false})
+	@Property({ nullable: false })
 	precioUnitario!: number;
 
-	@ManyToOne(() => Categoria, {nullable: false})
-	categoria!: Rel<Categoria>;
+	@Property({ nullable: true })
+	imagenes?: string[]; // Array of image URLs or file paths
 
-	@ManyToOne(() => Material, {nullable: false})
-	material!: Rel<Material>;
+	@ManyToOne(() => Categoria, { nullable: true }) // pueden existir muebles sin categoria
+	categoria?: Rel<Categoria> | null;
+
+	@ManyToOne(() => Material, { nullable: true }) // pueden existir muebles sin material
+	material?: Rel<Material> | null;
+
+	@ManyToMany(() => Cliente, (cliente) => cliente.favoritos)
+	favoritos = new Collection<Cliente>(this);
 
 	// lineaPedido es una entidad que representa una relacion con atributos
 	// oneToMany del lado del mueble
-	@ManyToOne(() => lineaPedido, {nullable: true})
+	@ManyToOne(() => lineaPedido, { nullable: true })
 	lineaPedido?: Rel<lineaPedido>;
-
-	/* @Property({nullable: true})
-      imagenes?: string[]; // Array of image URLs or file paths */
 }
