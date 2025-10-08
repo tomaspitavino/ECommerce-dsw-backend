263e55fb72e2c3973ba4c1cbaaf71d30ba4c6179
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-08 00:55:45 -0300

    Agregadas las fechas de creacion y update al base entity

--- src/shared/db/baseEntity.entity.mysql.ts
+++ src/shared/db/baseEntity.entity.mysql.ts
@@ -1,14 +1,12 @@
-import {DateTimeType, PrimaryKey, Property} from '@mikro-orm/core';
+import { DateTimeType, PrimaryKey, Property } from "@mikro-orm/core";
 
 export abstract class BaseEntity {
-	@PrimaryKey()
-	id?: number; // Usar number o string según la configuración de la base de datos
+  @PrimaryKey()
+  id?: number; // Usar number o string según la configuración de la base de datos
 
-	/*
-	@Property({type: DateTimeType})
-	createdAt?: Date = new Date();
+  @Property({ type: DateTimeType })
+  fechaCreacion?: Date = new Date();
 
-	@Property({type: DateTimeType, onUpdate: () => new Date()})
-	updatedAt?: Date = new Date();
-  */
+  @Property({ type: DateTimeType, onUpdate: () => new Date() })
+  fechaActualizado?: Date = new Date();
 }
