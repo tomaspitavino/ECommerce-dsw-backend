5b65d8a04395925a513597b3e2e8264406b79a15
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-08 21:09:56 -0300

    Solucionado un problema al usar id de categoria donde este era NaN para la BD

--- src/categoria/categoria.entity.mysql.ts
+++ src/categoria/categoria.entity.mysql.ts
@@ -1,26 +1,26 @@
 import {
-  Cascade,
-  Collection,
-  Entity,
-  OneToMany,
-  Property,
-} from "@mikro-orm/core";
-import { BaseEntity } from "../shared/db/baseEntity.entity.mysql.js";
-import { Mueble } from "../mueble/mueble.entity.mysql.js";
+	Cascade,
+	Collection,
+	Entity,
+	OneToMany,
+	Property,
+} from '@mikro-orm/core';
+import { Mueble } from '../mueble/mueble.entity.mysql.js';
+import { BaseEntity } from '../shared/db/baseEntity.entity.mysql.js';
 
 @Entity()
 export class Categoria extends BaseEntity {
-  @Property({ nullable: false, unique: true })
-  nombre!: string;
+	@Property({ nullable: false, unique: true })
+	nombre!: string;
 
-  @Property({ nullable: false })
-  descripcion!: string;
+	@Property({ nullable: false })
+	descripcion!: string;
 
-  @Property({ nullable: true })
-  imagen!: string; // URL or file path
+	@Property()
+	imagen!: string; // URL or file path
 
-  @OneToMany(() => Mueble, (mueble) => mueble.categoria, {
-    cascade: [Cascade.ALL],
-  })
-  muebles = new Collection<Mueble>(this);
+	@OneToMany(() => Mueble, (mueble) => mueble.categoria, {
+		cascade: [Cascade.ALL],
+	})
+	muebles = new Collection<Mueble>(this);
 }
