c6d363942754e0908818e2626ca025a634720419
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-10 12:41:29 -0300

    Mejor nombre para linea pedido (item)

--- src/pedido/pedido.entity.mysql.ts
+++ src/pedido/pedido.entity.mysql.ts
@@ -1,40 +1,40 @@
 import {
-	Cascade,
-	Collection,
-	Entity,
-	ManyToOne,
-	OneToMany,
-	Property,
-	Rel,
-} from '@mikro-orm/core';
-import { Cliente } from '../cliente/cliente.entity.mysql.js';
-import { Descuento } from '../descuento/descuento.entity.mysql.js';
-import { lineaPedido } from '../lineaPedido/lineaPedido.entity.mysql.js';
-import { BaseEntity } from '../shared/db/baseEntity.entity.mysql.js';
+  Cascade,
+  Collection,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  Property,
+  Rel,
+} from "@mikro-orm/core";
+import { Cliente } from "../cliente/cliente.entity.mysql.js";
+import { Descuento } from "../descuento/descuento.entity.mysql.js";
+import { Item } from "../item/item.entity.mysql.js";
+import { BaseEntity } from "../shared/db/baseEntity.entity.mysql.js";
 
 @Entity()
 export class Pedido extends BaseEntity {
-	@ManyToOne(() => Cliente, { nullable: false })
-	cliente!: Rel<Cliente>;
+  @ManyToOne(() => Cliente, { nullable: false })
+  cliente!: Rel<Cliente>;
 
-	// Estoy considerando que la entidad lineaPedido es una relación con atributos entre Cliente y Pedido
-	@OneToMany(() => lineaPedido, (linea) => linea.pedido, {
-		cascade: [Cascade.ALL],
-	})
-	lineas = new Collection<lineaPedido>(this);
+  // Estoy considerando que la entidad item es una relación con atributos entre Cliente y Pedido
+  @OneToMany(() => Item, (linea) => linea.pedido, {
+    cascade: [Cascade.ALL],
+  })
+  lineas = new Collection<Item>(this);
 
-	@Property()
-	fechaHora!: Date;
+  @Property()
+  fechaHora!: Date;
 
-	@Property({ default: 'pendiente' })
-	estado!: string;
+  @Property({ default: "pendiente" })
+  estado!: string;
 
-	@Property()
-	total!: number;
+  @Property()
+  total!: number;
 
-	@OneToMany(() => Descuento, (d) => d.pedido, {
-		cascade: [Cascade.PERSIST],
-		orphanRemoval: false, // no borra descuentos al eliminar pedido
-	})
-	descuentos = new Collection<Descuento>(this);
+  @OneToMany(() => Descuento, (d) => d.pedido, {
+    cascade: [Cascade.PERSIST],
+    orphanRemoval: false, // no borra descuentos al eliminar pedido
+  })
+  descuentos = new Collection<Descuento>(this);
 }
