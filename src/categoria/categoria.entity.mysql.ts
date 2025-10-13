6ffb6859e5aa1d2cecb54cc974a9c61b0562359d
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-12 21:07:18 -0300

    desaparecido grupo visual. Manejamos todo desde la misma categoria padre

--- src/categoria/categoria.entity.mysql.ts
+++ src/categoria/categoria.entity.mysql.ts
@@ -1,29 +1,26 @@
 import {
-  Cascade,
-  Collection,
-  Entity,
-  OneToMany,
-  Property,
-} from "@mikro-orm/core";
-import { Mueble } from "../mueble/mueble.entity.mysql.js";
-import { BaseEntity } from "../shared/db/baseEntity.entity.mysql.js";
+	Cascade,
+	Collection,
+	Entity,
+	OneToMany,
+	Property,
+} from '@mikro-orm/core';
+import { Mueble } from '../mueble/mueble.entity.mysql.js';
+import { BaseEntity } from '../shared/db/baseEntity.entity.mysql.js';
 
 @Entity()
 export class Categoria extends BaseEntity {
-  @Property({ nullable: false, unique: true })
-  nombre!: string;
+	@Property({ nullable: false, unique: true })
+	nombre!: string;
 
-  @Property({ nullable: false })
-  descripcion!: string;
+	@Property({ nullable: false })
+	descripcion!: string;
 
-  @Property()
-  imagen!: string; // URL or file path
+	@Property()
+	imagen!: string; // URL or file path
 
-  @Property({ nullable: true })
-  grupoVisual?: string;
-
-  @OneToMany(() => Mueble, (mueble) => mueble.categoria, {
-    cascade: [Cascade.ALL],
-  })
-  muebles = new Collection<Mueble>(this);
+	@OneToMany(() => Mueble, (mueble) => mueble.categoria, {
+		cascade: [Cascade.ALL],
+	})
+	muebles = new Collection<Mueble>(this);
 }
