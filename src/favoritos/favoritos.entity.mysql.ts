0f882f170b6b7e7c3eb22fe61465aa7c5b58ae1b
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-15 01:08:17 -0300

    Manejo de favoritos y sus rutas arregladas.

--- src/favoritos/favoritos.entity.mysql.ts
+++ src/favoritos/favoritos.entity.mysql.ts
@@ -1,13 +1,13 @@
-import { Entity, ManyToOne, Rel } from "@mikro-orm/core";
-import { Cliente } from "../cliente/cliente.entity.mysql.js";
-import { BaseEntity } from "../shared/db/baseEntity.entity.mysql.js";
-import { Mueble } from "../mueble/mueble.entity.mysql.js";
+import { Entity, ManyToOne, Rel } from '@mikro-orm/core';
+import { Cliente } from '../cliente/cliente.entity.mysql.js';
+import { Mueble } from '../mueble/mueble.entity.mysql.js';
+import { BaseEntity } from '../shared/db/baseEntity.entity.mysql.js';
 
 @Entity()
 export class Favorito extends BaseEntity {
-  @ManyToOne(() => Cliente)
-  cliente!: Rel<Cliente>;
+	@ManyToOne(() => Cliente)
+	cliente!: Rel<Cliente>;
 
-  @ManyToOne(() => Mueble)
-  mueble!: Rel<Mueble>;
+	@ManyToOne(() => Mueble)
+	mueble!: Rel<Mueble>;
 }
