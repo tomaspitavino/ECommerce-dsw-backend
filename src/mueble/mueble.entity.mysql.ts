87ecd983cf79cc4e3ef3a330fc832a712bb54e1d
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-18 22:36:59 -0300

    Actualiza la versión a 1.4.0 en package.json y agrega un nuevo endpoint PATCH para actualizar el estado de un pedido.

--- src/mueble/mueble.entity.mysql.ts
+++ src/mueble/mueble.entity.mysql.ts
@@ -1,49 +1,49 @@
 import {
-  Cascade,
-  Collection,
-  Entity,
-  ManyToOne,
-  OneToMany,
-  Property,
-  Rel,
-} from "@mikro-orm/core";
-import { Categoria } from "../categoria/categoria.entity.mysql.js";
-import { Favorito } from "../favoritos/favoritos.entity.mysql.js";
-import { Item } from "../item/item.entity.mysql.js";
-import { Material } from "../material/material.entity.mysql.js";
-import { BaseEntity } from "../shared/db/baseEntity.entity.mysql.js";
+	Cascade,
+	Collection,
+	Entity,
+	ManyToOne,
+	OneToMany,
+	Property,
+	Rel,
+} from '@mikro-orm/core';
+import { Categoria } from '../categoria/categoria.entity.mysql.js';
+import { Favorito } from '../favoritos/favoritos.entity.mysql.js';
+import { Item } from '../item/item.entity.mysql.js';
+import { Material } from '../material/material.entity.mysql.js';
+import { BaseEntity } from '../shared/db/baseEntity.entity.mysql.js';
 
 @Entity()
 export class Mueble extends BaseEntity {
-  @Property()
-  descripcion!: string;
+	@Property()
+	descripcion!: string;
 
-  @Property()
-  stock!: number;
+	@Property()
+	stock!: number;
 
-  @Property()
-  etiqueta!: string;
+	@Property()
+	etiqueta!: string;
 
-  @Property()
-  precioUnitario!: number;
+	@Property({ type: 'decimal', precision: 10, scale: 2 })
+	precioUnitario!: number;
 
-  @Property()
-  imagenes!: string[]; // Array of image URLs or file paths
+	@Property()
+	imagenes!: string[]; // Array of image URLs or file paths
 
-  @ManyToOne(() => Categoria)
-  categoria!: Rel<Categoria>;
+	@ManyToOne(() => Categoria)
+	categoria!: Rel<Categoria>;
 
-  @ManyToOne(() => Material)
-  material!: Rel<Material>;
+	@ManyToOne(() => Material)
+	material!: Rel<Material>;
 
-  // item es una entidad que representa una relacion con atributos
-  @OneToMany(() => Item, (item) => item.mueble, {
-    cascade: [Cascade.ALL],
-  })
-  items? = new Collection<Item>(this);
+	// item es una entidad que representa una relacion con atributos
+	@OneToMany(() => Item, (item) => item.mueble, {
+		cascade: [Cascade.ALL],
+	})
+	items? = new Collection<Item>(this);
 
-  @OneToMany(() => Favorito, (favorito) => favorito.mueble, {
-    cascade: [Cascade.REMOVE],
-  })
-  favoritos? = new Collection<Favorito>(this);
+	@OneToMany(() => Favorito, (favorito) => favorito.mueble, {
+		cascade: [Cascade.REMOVE],
+	})
+	favoritos? = new Collection<Favorito>(this);
 }
