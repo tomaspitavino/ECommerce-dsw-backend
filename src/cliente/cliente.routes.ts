51742c0fb2c3592bf1361989216e12831e353531
lordagustin22 <niconecrofilo589@gmail.com>
2025-10-20 03:41:07 -0300

    Ahora favoritos muestra toda la lista para un cliente, en vez de mostrar para todos

--- src/cliente/cliente.routes.ts
+++ src/cliente/cliente.routes.ts
@@ -5,6 +5,7 @@ import {
 	removeFavorito,
 	sanitizeFavoritoInput,
 } from '../favoritos/favoritos.controller.js';
+import { findOne as findOneMueble } from '../mueble/mueble.controller.js';
 import {
 	add,
 	findAll,
@@ -18,14 +19,12 @@ import {
 export const clienteRouter = Router();
 
 clienteRouter.get('/:id/favoritos', findAllFavoritos);
-// clienteRouter.get('/:id/favoritos/:idMueble', findOneFavorito); reemplazarla por la de mueble
+clienteRouter.get('/:id/favoritos/mueble/:idMueble', findOneMueble);
 clienteRouter.post('/:id/favoritos/', sanitizeFavoritoInput, addFavorito);
 clienteRouter.delete('/:id/favoritos/:muebleId', removeFavorito);
 
 // clienteRouter.post('/:id/favoritos/:muebleId', sanitizeFavoritoInput, addFavorito);
 // este approach requiere el id literal del mueble en el post de REST
-
-// necesito el delete de favoritos
 
 // CRUD independiente: /api/clientes
 clienteRouter.get('/', findAll);
--- src/item/item.routes.ts
+++ src/item/item.routes.ts
@@ -1,18 +1,19 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
-	add,
-	findAll,
-	findOne,
-	remove,
-	sanitizeItemInput,
-	update,
-} from './item.controller.js';
+  add,
+  findAll,
+  findOne,
+  remove,
+  sanitizeItemInput,
+  sanitizeItemPatchInput,
+  update,
+} from "./item.controller.js";
 
 export const itemRouter = Router();
 
-itemRouter.get('/', findAll);
-itemRouter.get('/:id', findOne);
-itemRouter.post('/', sanitizeItemInput, add);
-itemRouter.put('/:id', sanitizeItemInput, update);
-itemRouter.patch('/:id', sanitizeItemInput, update);
-itemRouter.delete('/:id', remove);
+itemRouter.get("/", findAll);
+itemRouter.get("/:id", findOne);
+itemRouter.post("/", sanitizeItemInput, add);
+itemRouter.put("/:id", sanitizeItemInput, update);
+itemRouter.patch("/:id", sanitizeItemPatchInput, update);
+itemRouter.delete("/:id", remove);
