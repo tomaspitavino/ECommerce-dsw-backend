723493d7a06abb28aaaf01d4db0eed5aa5d0c3d2
lordagustin22 <niconecrofilo589@gmail.com>
2026-04-21 16:13:42 -0300

    Utilizar la variable error para mostrar el tipo de error en consola

--- src/mueble/mueble.controller.ts
+++ src/mueble/mueble.controller.ts
@@ -1,77 +1,85 @@
-import { Request, Response } from 'express';
-import { orm } from '../shared/db/orm.js';
-import { validate } from '../shared/validation/validateRequest.js';
-import { MuebleSchema } from '../shared/validation/zodSchemas.js';
-import { Mueble } from './mueble.entity.mysql.js';
+import { Request, Response } from "express";
+import { orm } from "../shared/db/orm.js";
+import { validate } from "../shared/validation/validateRequest.js";
+import { MuebleSchema } from "../shared/validation/zodSchemas.js";
+import { Mueble } from "./mueble.entity.mysql.js";
 
 const em = orm.em;
 
 export const sanitizeMuebleInput = validate(MuebleSchema);
 
 export async function findAll(req: Request, res: Response) {
-	try {
-		const muebles = await em.find(
-			Mueble,
-			{},
-			{ populate: ['categoria', 'material'] }
-		);
-		res
-			.status(200)
-			.json({ Message: 'Todos los muebles encontrados', data: muebles });
-	} catch (error: any) {
-		res.status(500).json({ message: 'Error al cargar muebles' });
-	}
+  try {
+    const muebles = await em.find(
+      Mueble,
+      {},
+      { populate: ["categoria", "material"] },
+    );
+    res
+      .status(200)
+      .json({ Message: "Todos los muebles encontrados", data: muebles });
+  } catch (error: any) {
+    res.status(500).json({ message: "Error al cargar muebles", error: error });
+  }
 }
 
 export async function findOne(req: Request, res: Response) {
-	try {
-		const id = Number.parseInt(req.params.id);
-		const mueble = await em.findOneOrFail(
-			Mueble,
-			{ id },
-			{ populate: ['categoria', 'material'] }
-		);
-		res.status(200).json({ Message: 'Mueble encontrado', data: mueble });
-	} catch (error: any) {
-		res.status(500).json({ message: 'Error al cargar el mueble' });
-	}
+  try {
+    const id = Number.parseInt(req.params.id);
+    const mueble = await em.findOneOrFail(
+      Mueble,
+      { id },
+      { populate: ["categoria", "material"] },
+    );
+    res.status(200).json({ Message: "Mueble encontrado", data: mueble });
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: "Error al cargar el mueble", error: error });
+  }
 }
 
 export async function add(req: Request, res: Response) {
-	try {
-		const dto = req.body.validated;
-		const mueble = em.create(Mueble, dto);
+  try {
+    const dto = req.body.validated;
+    const mueble = em.create(Mueble, dto);
 
-		await em.flush();
-		res.status(200).json({ Message: 'Mueble creado', data: mueble });
-	} catch (error: any) {
-		res.status(500).json({ message: 'Error al crear el mueble' });
-	}
+    await em.flush();
+    res.status(200).json({ Message: "Mueble creado", data: mueble });
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: "Error al crear el mueble", error: error.name });
+  }
 }
 
 export async function update(req: Request, res: Response) {
-	try {
-		const id = Number.parseInt(req.params.id);
-		const dto = req.body.validated;
+  try {
+    const id = Number.parseInt(req.params.id);
+    const dto = req.body.validated;
 
-		const mueble = await em.findOneOrFail(Mueble, { id });
-		em.assign(mueble, dto);
+    const mueble = await em.findOneOrFail(Mueble, { id });
+    em.assign(mueble, dto);
 
-		await em.flush();
-		res.status(200).json({ Message: 'Mueble actualizado', data: mueble });
-	} catch (error: any) {
-		res.status(500).json({ message: 'Error al actualizar el mueble' });
-	}
+    await em.flush();
+    res.status(200).json({ Message: "Mueble actualizado", data: mueble });
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: "Error al actualizar el mueble", error: error });
+  }
 }
 
 export async function remove(req: Request, res: Response) {
-	try {
-		const id = Number.parseInt(req.params.id);
-		const mueble = await em.findOneOrFail(Mueble, { id });
+  try {
+    const id = Number.parseInt(req.params.id);
+    const mueble = await em.findOneOrFail(Mueble, { id });
 
-		await em.removeAndFlush(mueble);
-		res.status(200).json({ Message: 'Mueble eliminado', data: mueble });
-	} catch (error: any) {
-		res.status(500).json({ message: 'Error al eliminar el mueble' });
-	}
+    await em.removeAndFlush(mueble);
+    res.status(200).json({ Message: "Mueble eliminado", data: mueble });
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: "Error al eliminar el mueble", error: error });
+  }
 }
