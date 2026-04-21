93e91f1f264e9845bb5feb1e0a6d7539e94d6d66
lordagustin22 <niconecrofilo589@gmail.com>
2026-04-21 16:12:24 -0300

    Cambiar mensajes para que sean mas user friendly

--- src/auth/auth.middleware.ts
+++ src/auth/auth.middleware.ts
@@ -19,7 +19,9 @@ export function verifyToken(req: Request, res: Response, next: NextFunction) {
   const authHeader = req.headers.authorization;
 
   if (!authHeader?.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Token requerido" });
+    return res.status(401).json({
+      message: "Acceso denegado: Mala autenticación o falta de credenciales",
+    });
   }
 
   const token = authHeader.split(" ")[1];
@@ -29,14 +31,16 @@ export function verifyToken(req: Request, res: Response, next: NextFunction) {
     req.user = payload;
     next();
   } catch {
-    return res.status(401).json({ message: "Token inválido o expirado" });
+    return res.status(401).json({ message: "Sesión inválida o expirada" });
   }
 }
 
 export function requireRole(...roles: string[]) {
   return (req: Request, res: Response, next: NextFunction) => {
     if (!req.user || !roles.includes(req.user.rol)) {
-      return res.status(403).json({ message: "Acceso denegado" });
+      return res
+        .status(403)
+        .json({ message: "Acceso denegado: Mala autorización" });
     }
     next();
   };
