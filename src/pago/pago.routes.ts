8ea9008dc31e51cbb87c1e73c6013f6f2439d81b
lordagustin22 <niconecrofilo589@gmail.com>
2026-08-06 19:47:03 -0300

    Redirigir a mis pedidos despues de pago

--- src/pago/pago.routes.ts
+++ src/pago/pago.routes.ts
@@ -4,6 +4,10 @@ import { verifyToken } from "../auth/auth.middleware.js";
 
 export const pagoRouter = Router();
 
-pagoRouter.get("/success", (req, res) => res.send("Exito"));
+pagoRouter.get("/success", (req, res) => {
+  res.redirect(
+    `${process.env.FRONTEND_URL || "http://localhost:5173"}/pedidos`,
+  );
+});
 pagoRouter.post("/webhook", webhook); // público, lo llama MercadoPago
 pagoRouter.post("/:pedidoId", verifyToken, crearPreferencia);
